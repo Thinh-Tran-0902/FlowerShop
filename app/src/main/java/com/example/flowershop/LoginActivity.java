c559be1ef2d1f7d2e0e1ee0f3698083d4314d853
minh thuan <machao86960@gmail.com>
2023-03-02 13:36:37 +0700

    Login and SignUp

---
+++ app/src/main/java/com/example/flowershop/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.flowershop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.flowershop.models.DBHelper;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText username, password;
+    Button btnLogin , btnSignUpF;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username1);
+        password = findViewById(R.id.password1);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUpF = findViewById(R.id.btnSignUpF);
+        DB = new DBHelper(this);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)){
+                    Toast.makeText(LoginActivity.this, "All fields Required ", Toast.LENGTH_SHORT).show();
+
+                } else{
+                    Boolean checkUserPass = DB.checkUsernamePassword(user, pass);
+                    if(checkUserPass == false){
+                        Toast.makeText(LoginActivity.this, "Username or Password incorrect", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, ShopPageActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+
+        btnSignUpF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/flowershop/MainActivity.java
+++ app/src/main/java/com/example/flowershop/MainActivity.java
@@ -4,37 +4,99 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.flowershop.models.DBHelper;
+
+import java.nio.channels.InterruptedByTimeoutException;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText email, pass;
-    Button signinbutton;
+    EditText username, password, repassword;
+    Button signUp, signIn , btnSignInF;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassword);
+        signIn = findViewById(R.id.btnAccount);
+        signUp = findViewById(R.id.btnRegister);
+        btnSignInF = findViewById(R.id.btnLoginF);
+        DB = new DBHelper(this);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
 
-        email = (EditText) findViewById(R.id.textEmail);
-        pass = (EditText) findViewById(R.id.textPassword);
-        signinbutton = (Button) findViewById(R.id.buttonLogin);
+
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass= repassword.getText().toString();
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(repass)){
+                    Toast.makeText(MainActivity.this, "All fields Required", Toast.LENGTH_SHORT).show();
+                } else{
+                    if(pass.equals(repass)){
+                        Boolean checkUser = DB.checkUsername(user);
+                        if(checkUser == false){
+                            Boolean insert = DB.insertData(user,pass);
+                            if(insert == true){
+                                Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this,ShopPageActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Registered failed",Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this,"Username already existed!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(MainActivity.this, "Passowrd are not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        btnSignInF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
     }
 
-    public void login(View v){
-        String useremail = email.getText().toString();
-        String userpass = pass.getText().toString();
-
-        if(useremail.equals("tranthinh") && userpass.equals("12345")){
-            Intent in = new Intent(this, ShopPageActivity.class);
-            in.putExtra("username", useremail);
-            startActivity(in);
-        }else{
-            Toast.makeText(getBaseContext(), "Wrong email and password", Toast.LENGTH_SHORT).show();
-        }
-    }
+//    public void login(View v){
+//        String useremail = email.getText().toString();
+//        String userpass = pass.getText().toString();
+//
+//        if(useremail.equals("tranthinh") && userpass.equals("12345")){
+//            Intent in = new Intent(this, ShopPageActivity.class);
+//            in.putExtra("username", useremail);
+//            startActivity(in);
+//        }else{
+//            Toast.makeText(getBaseContext(), "Wrong email and password", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
 }
